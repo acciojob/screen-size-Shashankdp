f3291c6a2328413676d2b6782b04726e29029e86
Shashank Pattar <118378844+Shashankdp@users.noreply.github.com>
2023-05-30 20:45:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-//your JS code here. If required.
+//your JS code here. If r
+let newElement=document.createElement("h1");
+newElement.innerText='Width: ${window.innerWidth} and Height: ${window.innerHeight}';
+document.body.append(newElement);
+
+window.onresize=function(){
+	newElement.innerText='Width: ${window.innerWidth} and Height: ${window.innerHeight}';
+}
